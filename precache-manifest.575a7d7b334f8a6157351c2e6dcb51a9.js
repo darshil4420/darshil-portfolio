f903d14f09ffdfc8bc7bc909b1e8be5071ef8367
darshil4420 <darshilgshah83@gmail.com>
2020-04-20 15:40:25 +0530

    Updates

---
+++ precache-manifest.575a7d7b334f8a6157351c2e6dcb51a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f25eff96dbfff2fb603c",
+    "url": "/darshil-portfolio/static/css/main.523255b9.chunk.css"
+  },
+  {
+    "revision": "f25eff96dbfff2fb603c",
+    "url": "/darshil-portfolio/static/js/main.f25eff96.chunk.js"
+  },
+  {
+    "revision": "d6b15ae2c03f178c930c",
+    "url": "/darshil-portfolio/static/js/1.d6b15ae2.chunk.js"
+  },
+  {
+    "revision": "64a2ecd6ee4472623522",
+    "url": "/darshil-portfolio/static/js/runtime~main.64a2ecd6.js"
+  },
+  {
+    "revision": "dd3cfcc8f8104272a15258e45027ceff",
+    "url": "/darshil-portfolio/index.html"
+  }
+];
